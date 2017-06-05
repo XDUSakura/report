bfc09535e3770c133294e080b7a14f2945687f5b
Qinka <qinka@live.com>
2017-06-05 23:07:57 +0800

    update;tmp

--- integration/before.sh
+++ integration/before.sh
@@ -5,16 +5,16 @@
 ##
 
 ## update apt
-sudo apt update
+apt update
 ## install
-sudo apt install -y --no-install-recommends wget texlive-base texlive-xetex latex-xcolor xzdec \
+apt install -y --no-install-recommends wget texlive-base texlive-xetex latex-xcolor xzdec \
   texlive-fonts-recommended texlive-latex-extra lmodern texlive-latex-recommended
 # create texmf
 cd ~ && mkdir texmf
 tlmgr init-usertree
 # update packages
-sudo tlmgr update --all
-sudo tlmgr install ctex l3kernel xecjk l3packages latex
-sudo tlmgr install geometry fontspec fandol ulem zhnumber
+tlmgr update --all
+tlmgr install ctex l3kernel xecjk l3packages latex
+tlmgr install geometry fontspec fandol ulem zhnumber
 # return to work path
 cd $TRAVIS_BUILD_DIR
